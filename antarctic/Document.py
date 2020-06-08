5d25dfaefc970a82dcd6ecf7d7c96c663ce1d978
Thomas Schmelzer <thomas.schmlezer@gmail.com>
2020-06-08 23:28:18 +0200

    XDocument

---
+++ antarctic/Document.py
@@ -0,0 +1,60 @@
+import pandas as pd
+from mongoengine import *
+
+
+class XDocument(Document):
+    # A XDocument is an abstract Mongo Document, e.g. instances of this document can not be instantiated.
+    # All concrete objects such as Symbols or Strategies are children of the XDocument.
+    # Having a common parent helps to share functionality
+    meta = {'abstract': True}
+
+    name = StringField(unique=True, required=True)
+    reference = DictField()
+
+    @classmethod
+    def reference_frame(cls, products=None) -> pd.DataFrame:
+        products = products or cls.objects
+
+        frame = pd.DataFrame(
+            {product.name: pd.Series({key: data for key, data in product.reference.items()}) for product in
+             products}).transpose()
+        frame.index.name = cls.__name__.lower()
+        return frame.sort_index()
+
+    @classmethod
+    def products(cls, names=None):
+        # extract symbols from database
+        if names is None:
+            return cls.objects
+        else:
+            return cls.objects(name__in=names)
+
+    @classmethod
+    def to_dict(cls):
+        # represent all documents of a class as a dictionary
+        return {x.name: x for x in cls.objects}
+
+    @classmethod
+    def apply(cls, f, products=None, default=None) -> pd.DataFrame:
+        products = products or cls.objects
+        default = default or pd.Series({})
+        for product in products:
+            try:
+                yield product.name, f(product) #or default
+            except:
+                yield product.name, default
+
+    def __lt__(self, other):
+        # sort documents by name
+        return self.name < other.name
+
+    def __eq__(self, other):
+        # two documents are the sname if they have the same name and class
+        return self.__class__ == other.__class__ and self.name == other.name
+
+    # we want to make a set of assets, etc....
+    def __hash__(self):
+        return hash(self.to_json())
+
+    def __str__(self):
+        return "<{type}: {name}>".format(type=self.__class__.__name__, name=self.name)
---
+++ test/test_document.py
@@ -0,0 +1,132 @@
+import numpy as np
+import pandas as pd
+
+import pytest
+from mongoengine import connect, NotUniqueError
+from mongomock.gridfs import enable_gridfs_integration
+
+from antarctic.Document import XDocument
+from antarctic.PandasFields import SeriesField
+
+enable_gridfs_integration()
+
+client = connect(db="test", host="mongomock://localhost")
+
+
+class Singer(XDocument):
+    price = SeriesField()
+
+
+def test_reference_frame():
+    # can't harm to clean a bit
+    Singer.objects.delete()
+
+    p1 = Singer(name="Peter")
+    p1.reference["A"] = 20.0
+    p1.save()
+
+    p2 = Singer(name="Falco")
+    p2.reference["A"] = 30.0
+    p2.reference["B"] = 10.0
+    p2.save()
+
+    frame = Singer.reference_frame(products=[p1, p2])
+    assert set(frame.index) == {"Peter", "Falco"}
+    assert set(frame.keys()) == {"A", "B"}
+
+    frame = Singer.reference_frame()
+    assert set(frame.index) == {"Peter", "Falco"}
+    assert set(frame.keys()) == {"A", "B"}
+
+
+def test_lt():
+    assert Singer(name="A") < Singer(name="B")
+
+
+def test_reference():
+    p = Singer(name="Peter Maffay")
+    assert p.reference.get("NoNoNo", default=5) == 5
+
+    p.reference["XXX"] = 10
+    assert p.reference.keys() == {"XXX"}
+    assert {k: v for k, v in p.reference.items()} == {"XXX": 10}
+
+
+def test_equals():
+    # can't harm to clean a bit
+    Singer.objects.delete()
+
+    p1 = Singer(name="Peter Maffay")
+    p2 = Singer(name="Peter Maffay")
+
+    assert p1 == p2
+
+
+def test_products():
+    # can't harm to clean a bit
+    Singer.objects.delete()
+
+    p1 = Singer(name="Peter").save()
+    p2 = Singer(name="Falco").save()
+
+    # here we query the database! Hence need the client in the background
+    a = Singer.products(names=["Peter"])
+    assert len(a) == 1
+    assert a[0] == p1
+
+    b = Singer.products()
+    assert len(b) == 2
+    assert set(b) == {p1, p2}
+
+    frame = Singer.reference_frame(products=[p1, p2])
+    assert set(frame.index) == {"Peter", "Falco"}
+    assert frame.empty
+
+    frame = Singer.reference_frame()
+    assert set(frame.index) == {"Peter", "Falco"}
+    assert frame.empty
+
+
+def test_repr():
+    p1 = Singer(name="Peter Maffay")
+    assert str(p1) == "<Singer: Peter Maffay>"
+
+
+def test_not_unique_name():
+    # can't harm to clean a bit
+    Singer.objects.delete()
+
+    # check that no singer has survived
+    s = [singer for singer in Singer.objects]
+    assert len(s) == 0
+
+    c1 = Singer(name="AA").save()
+    assert c1.name == "AA"
+
+    # try to create a second singer with the same name
+    with pytest.raises(NotUniqueError):
+        Singer(name="AA").save()
+
+
+def test_to_dict():
+    # can't harm to clean a bit
+    Singer.objects.delete()
+
+    c1 = Singer(name="AAA").save()
+    c2 = Singer(name="BBB").save()
+
+    assert Singer.to_dict() == {"AAA": c1, "BBB": c2}
+
+
+def test_apply():
+    Singer.objects.delete()
+    s1 = Singer(name="Falco").save()
+    s2 = Singer(name="Peter Maffay").save()
+
+    s1.price = pd.Series(index=[1,2,3], data=[7.0,9.0,8.0])
+    s2.price = pd.Series(index=[1,3], data=[8.0, 10.0])
+    s1.save()
+    s2.save()
+
+    a = {name: value for name, value in Singer.apply(f=lambda x: x.price.mean(), default=np.nan)}
+    assert a == {"Falco": 8.0, "Peter Maffay": 9.0}
