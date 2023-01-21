3b1e949b212ae1078b4f6ddef81da06d0f82bfff
Thomas Schmelzer <thomas.schmelzer@gmail.com>
2023-01-21 20:55:26 +0400

    Update main.yml (#40)
    * Update main.yml
    
    * Update __init__.py
    
    * Create github_actions_demo.yml
    
    * Update main.yml
    
    * Update __init__.py
    
    * Update main.yml
    
    * Update document.py
    
    * Format code with black and isort
    
    * Update pandas_field.py
    
    * Format code with black and isort
    
    * Create gitlab_demo.yml (#41) (#42)
    
    * Create gitlab_demo.yml (#41) (#43)
    
    * Update document.py
    
    * Format code with black and isort
    
    * Delete github_actions_demo.yml
    
    * Update __init__.py
    
    * Delete .github/workflow directory
    
    * Update __init__.py
    
    * Update __init__.py
    
    Co-authored-by: deepsource-autofix[bot] <62050782+deepsource-autofix[bot]@users.noreply.github.com>

--- antarctic/document.py
+++ antarctic/document.py
@@ -1,3 +1,5 @@
+"""overload the document class of MongoEngine"""
+
 from datetime import datetime
 
 import pandas as pd
@@ -22,6 +24,7 @@ class XDocument(Document):
 
     @classmethod
     def reference_frame(cls, objects=None) -> pd.DataFrame:
+        """get a frame of reference data for each object"""
         objects = objects or cls.objects
 
         frame = pd.DataFrame(
@@ -35,7 +38,7 @@ class XDocument(Document):
 
     @classmethod
     def subset(cls, names=None):
-        # extract symbols from database
+        """extract a subset of documents from the database"""
         if names is None:
             return cls.objects
 
@@ -43,12 +46,18 @@ class XDocument(Document):
 
     @classmethod
     def to_dict(cls, objects=None):
+        """create a dictionary of objects of class cls
+        The objects are either given explicitly or if not
+        all objects of this particular class are extracted.
+        """
         # represent all documents of a class as a dictionary
         objects = objects or cls.objects
         return {x.name: x for x in objects}
 
     @classmethod
     def apply(cls, func, default, objects=None) -> pd.DataFrame:
+        """apply a function func to documents.
+        Yield the default document if something went wrong"""
         objects = objects or cls.objects
 
         for obj in objects:
@@ -59,25 +68,32 @@ class XDocument(Document):
 
     @classmethod
     def frame(cls, series, objects=None) -> pd.DataFrame:
+        """get a series from each document and return a frame of them"""
         objects = objects or cls.objects
         return pd.DataFrame({p.name: getattr(p, series) for p in objects}).dropna(
             axis=1, how="all"
         )
 
     def __lt__(self, other):
-        # sort documents by name
+        """sort documents by name"""
         return self.name < other.name
 
     def __eq__(self, other):
+        """two documents are equal if they are of the same class
+        and have the same name"""
+
         # two documents are the sname if they have the same name and class
         return self.__class__ == other.__class__ and self.name == other.name
 
     # we want to make a set of assets, etc....
     def __hash__(self):
+        """hashcode"""
         return hash(self.to_json())
 
     def __str__(self):
+        """string representation of a document"""
         return f"<{self.__class__.__name__}: {self.name}>"
 
     def __repr__(self):
+        """representation of a document"""
         return f"<{self.__class__.__name__}: {self.name}>"
