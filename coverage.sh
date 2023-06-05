e0e758db8c4fd8186b0618c62142341ede9d6f42
Thomas Schmelzer <thomas.schmelzer@gmail.com>
2023-06-05 11:42:09 -0700

    coverage

--- coverage.sh
+++ coverage.sh
@@ -2,4 +2,11 @@
 poetry run coverage run --source=antarctic/. -m pytest
 poetry run coverage report -m
 poetry run coverage html
-open htmlcov/index.html
+if [[ $OSTYPE == 'darwin'* ]]; then
+    open htmlcov/index.html
+fi
+if [[ $OSTYPE == 'linux'* ]]; then
+    xdg-open htmlcov/index.html 2> /dev/null
+fi
+
+
