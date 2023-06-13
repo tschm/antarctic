a6a7fcb491a38a2b648370acd7b540b2c1231880
Thomas Schmelzer <thomas.schmelzer@gmail.com>
2023-06-13 10:11:44 -0700

    Update conftest.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -19,15 +19,13 @@ def resource_fixture():
 def client_fixture():
     """database fixture"""
     db_name = "test"
-    connection = connect(db_name)
+    with connect(db_name) as connection:
+        db = connection.get_database(db_name)
+        for collection in db.list_collection_names():
+            print(collection)
+            print(type(collection))
+            col = db[collection]
+            col.delete_many({})
     
-    db = connection.get_database(db_name)
-    
-    for collection in db.list_collection_names():
-        print(collection)
-        print(type(collection))
-        col = db[collection]
-        col.delete_many({})
-    
-    yield connection
+    yield connect()
     disconnect()
