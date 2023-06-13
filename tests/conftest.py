1952b14c7157e45ae01a7275cb82198c54519cb2
Thomas Schmelzer <thomas.schmelzer@adia.ae>
2023-06-12 20:37:29 -0700

    demo notebook

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,10 +4,11 @@ from __future__ import annotations
 
 from pathlib import Path
 
-import mongomock
 import pytest
 from mongoengine import connect
 from mongoengine import disconnect
+
+# import mongomock
 
 
 @pytest.fixture(scope="session", name="resource_dir")
@@ -19,6 +20,6 @@ def resource_fixture():
 @pytest.fixture(scope="function", name="client")
 def client_fixture():
     """database fixture"""
-    yield connect(db="test_pandas", mongo_client_class=mongomock.MongoClient)
+    yield connect(db="test_pandas")  # , mongo_client_class=mongomock.MongoClient)
 
     disconnect()
