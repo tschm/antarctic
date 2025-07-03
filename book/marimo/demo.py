dc82fcceb514c38dd2f35b8bf26fa40d2816d059
Thomas Schmelzer <thomas.schmelzer@gmail.com>
2025-07-03 08:10:05 +0200

    Update demo.py

--- book/marimo/demo.py
+++ book/marimo/demo.py
@@ -1,24 +1,31 @@
+# /// script
+# requires-python = ">=3.12"
+# dependencies = [
+#     "marimo==0.14.9",
+#     "pandas==2.3.0",
+#     "polars==1.31.0",
+#     "pyarrow==20.0.0",
+# ]
+# ///
+
 import marimo
 
 __generated_with = "0.10.10"
 app = marimo.App()
 
-
-@app.cell
-def _(mo):
-    mo.md(r"""# Demo""")
-    return
+with app.setup:
+    import marimo as mo
+    import pandas as pd
 
 
 @app.cell
 def _():
-    import pandas as pd
     from mongoengine import Document, connect
     from mongomock import MongoClient
 
     # connect with your existing MongoDB (here I am using a popular interface mocking a MongoDB)
     client = connect(db="test", mongo_client_class=MongoClient)
-    return Document, MongoClient, client, connect, pd
+    return Document, MongoClient, client, connect
 
 
 @app.cell
@@ -39,21 +46,21 @@ def _(Document, PandasField):
 
 
 @app.cell
-def _(pd):
+def _():
     ts = pd.read_csv("public/ts.csv", index_col=0, parse_dates=True)
     print(ts)
     return (ts,)
 
 
 @app.cell
-def _(pd):
+def _():
     prices = pd.read_csv("public/price.csv", index_col=0, parse_dates=True, header=0)
     print(prices)
     return (prices,)
 
 
 @app.cell
-def _(Portfolio, pd, prices, ts):
+def _(Portfolio, prices, ts):
     portfolio = Portfolio(
         nav=ts,
         prices=prices,
@@ -61,13 +68,6 @@ def _(Portfolio, pd, prices, ts):
     )
     portfolio.save()
     return (portfolio,)
-
-
-@app.cell
-def _():
-    import marimo as mo
-
-    return (mo,)
 
 
 if __name__ == "__main__":
