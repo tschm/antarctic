775b3249ce11054f01b17d71518d593e2a03cc02
Thomas Schmelzer <thomas.schmelzer@gmail.com>
2025-12-07 23:14:24 +0400

    fix test location

--- tests/__init__.py
+++
@@ -1,6 +0,0 @@
-"""Test package for the Antarctic library.
-
-This package contains tests for the Antarctic library, which provides tools for
-storing and retrieving pandas DataFrames in MongoDB. The tests verify the functionality
-of the library's components, including the PandasField and XDocument classes.
-"""
